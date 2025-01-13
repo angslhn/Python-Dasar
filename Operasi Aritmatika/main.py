12f86c7a51b4adb1e62a3cbe0240c2048180dd14
angslhn <angslhn@gmail.com>
2025-01-14 06:18:49 +0700

    Operasi aritmatika.

---
+++ Operasi Aritmatika/main.py
@@ -0,0 +1,25 @@
+# Operasi aritmatika pada python 
+
+# Tambah
+a = 10 + 20
+
+# Kurang
+b = 20 - 10
+
+# Kali
+c = 15 * 10
+
+# Bagi
+d = 15 / 5
+
+# Eksponen (Pangkat)
+e = 15 ** 3
+
+# Modulus (Sisa bagi)
+f = 15 % 3
+
+# Floor division (Di bulatkan ke bawah)
+g = 15 // 4
+
+# Urutan operasi matematika
+# () ---> ** ---> * atau / atau % atau // ---> + atau - 

99fc1068e94e0610148a0b57e7e7d3586615a0ee
angslhn <angslhn@gmail.com>
2025-01-14 05:59:59 +0700

    Merubah atau konversi tipe data.

---
+++ Casting Tipe Data/main.py
@@ -0,0 +1,26 @@
+# Merubah atau konversi tipe data
+a = ""
+x = 10
+y = 15.5
+z = "10"
+
+# Merubah atau konversi ke integer
+data_x_integer = int(x)
+data_y_integer = int(y)
+data_z_integer = int(z)
+
+# Merubah atau konversi ke float
+data_x_float = float(x)
+data_y_float = float(y)
+data_z_float = float(z)
+
+# Merubah atau konversi ke string
+data_x_str = str(x)
+data_y_str = str(y)
+data_z_str = str(z)
+
+# Merubah atau konversi ke boolean
+data_a_boolean = bool(a)
+data_x_boolean = bool(x)
+data_y_boolean = bool(y)
+data_z_boolean = bool(z)

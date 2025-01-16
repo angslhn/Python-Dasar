82326d010949b923e7d7f51b987c3abe64f5128e
angslhn <angslhn@gmail.com>
2025-01-16 09:05:30 +0700

    Operator Assignment.

---
+++ Operator Assignment/main.py
@@ -0,0 +1,31 @@
+# Operator assignment
+
+a = 5
+
+# Tambah
+a += 5 # ini sama dengan a = a + 5
+print('Hasilnya adalah', a)
+
+# Kurang
+a -= 3 # ini sama dengan a = a - 3
+print('Hasilnya adalah', a)
+
+# Kali
+a *= 10 # ini sama dengan a = a x 5
+print('Hasilnya adalah', a)
+
+# Bagi
+a /= 2 # ini sama dengan a = a : 5
+print('Hasilnya adalah', a)
+
+# Eksponen (Pangkat)
+a **= 5 # ini sama dengan a = a ** 5
+print('Hasilnya adalah', a)
+
+# Floor division (Di bulatkan ke bawah)
+a //= 5 # ini sama dengan a = a // 5
+print('Hasilnya adalah', a)
+
+# Modulus (Sisa bagi)
+a %= 5 # ini sama dengan a = a % 5
+print('Hasilnya adalah', a)

e28076cf4d8e664d906a8d1de8b422e911c250db
angslhn <angslhn@gmail.com>
2025-02-02 09:39:24 +0700

    Tuple dan Set.

---
+++ Tuple dan Set/main.py
@@ -0,0 +1,12 @@
+# Tuple dan Set
+
+# Tuple
+# Immutable atau tidak bisa diubah dan menambah elemen
+angka = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+print(angka)
+
+# Set
+# Tidak berurutan atau tanpa index dan tidak menerima duplikasi serta bisa menambahkan elemen
+angka = {1, 1, 1, 2, 3, 3, 4, 4, 4, 4}
+angka.add(5)
+print(angka)
